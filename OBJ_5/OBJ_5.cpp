80ff69c64f3b79ec8691e1ad5fa9807efaf856b7
Effanuel <32594043+Effanuel@users.noreply.github.com>
2019-05-19 22:26:07 +0300

    interface update, removed struct

--- OBJ_5/OBJ_5.cpp
+++ OBJ_5/OBJ_5.cpp
@@ -1,97 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <map>
-#include <iterator>
-#include <sstream>
-#include <algorithm>
-
-using std::string;
-using std::vector;
-using std::cout;
-using std::endl;
-using std::map;
-
-typedef unsigned int uint;
-
-struct Words {
-	uint count;
-	vector<uint> lines;
-};
-
-
-typedef map<string, Words> MAPw;
-
-
-
-bool wordInDict(MAPw& mapOfWords, string& val)
-{
-	MAPw::iterator it = mapOfWords.begin();
-	while (it != mapOfWords.end())
-	{
-		if (it->first == val)
-			return true;
-		++it;
-	}
-	return false;
-}
-
-void incrementMap(MAPw& wordDict, string& word, uint& index) {
-	MAPw::iterator it = wordDict.begin();
-	while (it != wordDict.end())
-	{
-		if (it->first == word) {
-			++(it->second.count);
-			it->second.lines.push_back(index);
-			break;
-		}
-		++it;
-	}
-}
-
-void cleanString(string& line) {
-	vector<char> symbols{ '"', '„', '“', '…', '(', ')', '\\','—','-',',', '.', '?', '!', ':', ';', '\'', '{', '}', '*', '#', '[', ']' };
-	std::transform(line.begin(), line.end(), line.begin(), ::tolower);
-	for (auto& a : line) {
-		for (const auto& symbol : symbols) {
-			if (a == symbol)
-				a = ' ';
-		}
-	}
-}
-
-
-
-MAPw readFile() {
-	
-	MAPw wordDict;
-	std::ifstream failas("SAMPLE.txt");
-
-	string line;
-
-	for (uint i = 0; !failas.eof(); ++i) {
-		std::getline(failas, line);
-		cleanString(line);
-		std::stringstream ss(line);
-
-		string word;
-		while (ss >> word) {
-			if (!wordInDict(wordDict, word)) {
-				Words init{ 1, { i } };
-				wordDict.insert(MAPw::value_type(word, init));
-			}
-			else {
-				incrementMap(wordDict, word, i);
-			}
-		}
-	}
-	return wordDict;
-}
-
-
-
-
+#include "functions.h"
 
 
 int main()
@@ -99,19 +6,23 @@ int main()
 	MAPw wordDict = readFile();
 
 	MAPw::iterator it = wordDict.begin();
-	int c = 0;
-	while (it != wordDict.end())
-	{
-		if (it->second.count) {
-			std::cout << it->first << "\t:: " << it->second.count << " ::\t";
-			for (const auto& a : it->second.lines) {
-				cout << a << " ";
+	try {
+		if (it->second.size() == 0) throw std::runtime_error("Teksto failas yra tuscias");
+		for(const auto& x : wordDict)
+		{
+			if (x.second.size()) {
+				std::cout <<"Zodis "<< x.first << " buvo pakartotas " << x.second.size() << " karta(-us) eilutese: ";
+				for (const auto& a : x.second) {
+					cout << a << " ";
+				}
+				cout << endl;
 			}
-			cout << endl;
-		}
 
-		it++;
-		++c;
+			++it;
+		}
 	}
-	cout << c << endl;
+	catch (const std::runtime_error& e) {
+		cout << e.what() << endl;
+	}
+
 }
---
+++ OBJ_5/functions.h
@@ -0,0 +1,87 @@
+﻿#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <iterator>
+#include <sstream>
+#include <algorithm>
+#include <exception>
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::endl;
+using std::map;
+
+typedef unsigned int uint;
+typedef map<string, vector<uint>> MAPw;
+
+
+
+bool wordInDict(MAPw& mapOfWords, string& val)
+{
+	MAPw::iterator it = mapOfWords.begin();
+	while (it != mapOfWords.end())
+	{
+		if (it->first == val)
+			return true;
+		++it;
+	}
+	return false;
+}
+
+void incrementMap(MAPw & wordDict, string & word, uint & index) {
+	MAPw::iterator it = wordDict.begin();
+	while (it != wordDict.end())
+	{
+		if (it->first == word) {
+			it->second.push_back(index + 1);
+			break;
+		}
+		++it;
+	}
+}
+
+void cleanString(string & line) {
+	vector<char> symbols{ '"', '„', '“', '…', '(', ')', '\\','—','-',',', '.', '?', '!', ':', ';', '\'', '{', '}', '*', '#', '[', ']' };
+	std::transform(line.begin(), line.end(), line.begin(), ::tolower);
+	for (auto& a : line) {
+		for (const auto& symbol : symbols) {
+			if (a == symbol)
+				a = ' ';
+		}
+	}
+}
+
+
+
+MAPw readFile() {
+
+	MAPw wordDict;
+	std::ifstream failas("SAMPLE.txt");
+
+	string line;
+
+	for (uint i = 0; !failas.eof(); ++i) {
+		std::getline(failas, line, '\n');
+		cout << line << endl;
+		cleanString(line);
+		std::stringstream ss(line);
+
+		string word;
+		while (ss >> word) {
+			if (!wordInDict(wordDict, word)) {
+				vector<uint> init{ 1 };
+				wordDict.insert(MAPw::value_type(word, init));
+			}
+			else {
+				incrementMap(wordDict, word, i);
+			}
+		}
+		line.clear();
+	}
+	return wordDict;
+}
+
